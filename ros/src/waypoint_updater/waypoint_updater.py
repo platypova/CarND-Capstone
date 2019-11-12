e148fa419ad6508438207913056eedd402f189a7
platypova <p.latypova@gmail.com>
2019-11-12 11:37:51 +0000

    Waypoint updater first part - OK

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -33,28 +33,32 @@ class WaypointUpdater(object):
 
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-        # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-        rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
-        rospy.Subscriber('/obstacle_waypoint', Int32, self.obstacle_cb)
+        
+        # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below:
+        #rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
+        #rospy.Subscriber('/obstacle_waypoint', Int32, self.obstacle_cb)
 
-        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
+        self.final_waypoints_pub = rospy.Publisher('/final_waypoints', Lane, queue_size=1)
 
-        # TODO: Add other member variables you need below
+        # TODO: Add other member variables you need below:
+        self.waypoint = None
+        self.current_pose = None
         self.pose = None
         self.base_waypoints = None
         self.waypoints_2d = None
         self.waypoint_tree = None
-	    #self.stopline_wp_idx = -1
+        self.stopline_wp_idx = -1
         self.loop()
+        
         #rospy.spin()
         
     def loop(self):
         rate = rospy.Rate(50)
         while not rospy.is_shutdown():
-            if self.pose and self.base_waypoints:
+            if self.pose and self.base_waypoints and self.waypoint_tree:
                 #Get closest waypoint
                 closest_waypoint_idx = self.get_closest_waypoint_idx()
-                self.publish_waypoints()
+                self.publish_waypoints(closest_waypoint_idx)
             rate.sleep()
     
     def get_closest_waypoint_idx(self):
